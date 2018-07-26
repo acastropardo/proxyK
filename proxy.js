044aec68695b37ce9fcad9fba399156d82eae994
Abraham Castro <acastropardo@Abrahams-MacBook-Pro.local>
2018-07-25 23:35:13 -0400

    noche avance inicial

--- proxy.js
+++ proxy.js
@@ -8,17 +8,44 @@ var request = require('request');
 
 var login = 'Basic YWNhc3RybzpQaHIyMDE4Iw==';
 var urlPeriodos = 'http://vm-kfmfiori.grupokaufmann.com:8000/sap/opu/odata/sap/ZHR_TEST_PDF_SRV/periodosCollection?$format=json';
-var urlPDF = "http://vm-kfmfiori.grupokaufmann.com:8000/sap/opu/odata/sap/ZHR_TEST_PDF_SRV/boletaPagoCollection(Pernr='30009203',Pabrp='01',Pabrj='2015')";
+var urlPDF = "http://vm-kfmfiori.grupokaufmann.com:8000/sap/opu/odata/sap/ZHR_TEST_PDF_SRV/boletaPagoCollection(Pernr='30009203',Pabrp='01',Pabrj='2015')?$format=json";
+var urlPDFBase64 = "http://vm-kfmfiori.grupokaufmann.com:8000/sap/opu/odata/sap/ZHR_TEST_PDF_SRV/empleadoCollection(Employeenumber='30009203',Pabrp='01',Pabrj='2015')/$value";
 var empleadoTemp = '30009203';
 var pabrp;
 var pabrj;
 var pernr;
 
+//app.use('/', express.static('static'));
+
+  var base64ToBuffer = function(base64) {
+
+        var byteString = new Buffer(base64, 'base64').toString('binary');
+
+        var ab = new Buffer(byteString.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        return ab;
+  }
+
+  function parsePDF(stringValue) {
+
+       var string = JSON.stringify(stringValue);
+       var objectValue = JSON.parse(string);
+
+       return objectValue['pdf'];
+    }
+
 
 router.get('/pdf', function(req, res){
+
+
 //Lets configure and request
 request({
     url: urlPDF, //URL to hit
+    encoding: null,
     //qs: {from: 'example', time: +new Date()}, //Query string data
     method: 'GET', // specify the request type
     headers: { // speciyfy the headers
@@ -31,18 +58,36 @@ request({
         console.log('Hora: ', Date.now());
         console.log(error);
     } else {
-        console.log('Hora: ', Date.now());
-        console.log(response.statusCode);
-        console.log(body);
+        //console.log('Hora: ', Date.now());
+        //console.log(response.statusCode);
+        //console.log(body);
 
-        //var username = JSON.parse(body).username;
-        //console.log(body.username);
+        var pdf = parsePDF(body);
+        console.log(pdf);
+
+        
         res.setHeader('Content-Type', 'application/json');
-        res.send(body);
+        //res.setHeader('Content-Type', 'application/pdf');
+        
+        /*var stream = require('stream');
+
+        // Initiate the source
+        var bufferStream = new stream.PassThrough();
+
+        // Write your buffer
+        bufferStream.end(new Buffer(body));
+
+        // Pipe it to something else  (i.e. stdout)
+        bufferStream.pipe(res)
+        //res.pipe(body);*/
+        //var buff = base64ToBuffer(body);
+        res.send(pdf);
+
     }
 
 
 });
+
 });
 
 router.get('/periodos', function(req, res){
@@ -74,4 +119,32 @@ request({
 });
 });
 
+router.get('/pdfBase64', function(req, res){
+//Lets configure and request
+request({
+    url: urlPDFBase64, //URL to hit
+    //qs: {from: 'example', time: +new Date()}, //Query string data
+    method: 'GET', // specify the request type
+    headers: { // speciyfy the headers
+        'Authorization': login,
+        'Custom-Header': 'Custom Value'
+    },
+    //body: 'Hello Hello! String body!' //Set the body as a string
+}, function(error, response, body){
+    if(error) {
+        console.log('Hora: ', Date.now());
+        console.log(error);
+    } else {
+        //console.log(response.statusCode);
+        //console.log(body);
+
+        //var username = JSON.parse(body).username;
+        //console.log(body.username);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(body);
+    }
+
+
+});
+});
 module.exports = router;
